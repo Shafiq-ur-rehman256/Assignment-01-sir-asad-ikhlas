03ae36131506d02c0ec8c2694988ef58aadba6ef
FS.Code*shafiq <shafiqurrehman45@yahoo.com>
2020-09-08 15:09:38 +0500

    task 7 complete

---
+++ task 7/task7.js
@@ -0,0 +1,10 @@
+var PhoneNumber = "03205757562";
+var Nic = 4250187968352;
+var Name = "umair"
+
+Nic = Nic.toString();
+Name = `Mr.${Name}`;
+
+console.log(`${PhoneNumber} and its data type ${typeof(Number(PhoneNumber))}`);
+console.log(`Nic is ${Nic}`);
+console.log(Name);
